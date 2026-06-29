ecdf49b17ecabbc8d373909cfffc912e9112a534
mrudulakanduri <mrudulakanduri@gmail.com>
2026-06-29 15:03:36 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    int[] dp;
+
+    public int climbStairs(int n) {
+        if (dp == null) {
+            dp = new int[n + 1];  // initialize only once
+        }
+        if (n == 0) return 1;
+        if (n < 0)  return 0;
+        if (dp[n] != 0) return dp[n];
+
+        dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return dp[n];
+    }
+}
